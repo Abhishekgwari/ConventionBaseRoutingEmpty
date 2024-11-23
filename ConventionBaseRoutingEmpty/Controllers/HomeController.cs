a256f5b2418b638bc1c8712bc62cb0615346d0f8
Abhishek <Abhishekgwari14sep@gmail.com>
2024-11-23 11:45:24 +0530

    Attribute Base Routing

--- ConventionBaseRoutingEmpty/Controllers/HomeController.cs
+++ ConventionBaseRoutingEmpty/Controllers/HomeController.cs
@@ -2,20 +2,35 @@
 
 namespace ConventionBaseRoutingEmpty.Controllers
 {
+    // [Route("Home")] //  if we write here controller name here so 
+    //   in below we dont have to write controller name
+    //  we have to write action only
+    //  
+    //  [Route("Controller")] // tokens
+ [Route("[Controller]/[action]")]// tokens so we dont have to reapet
     public class HomeController : Controller
     {
+        [Route("")]          //  Empty route
+        [Route("Home")]
+        [Route("Home/Index")]
+        //      [Route("Index")]   // index action methods 
+
+
+        // [Route("Action")]  // tokens 
         public IActionResult Index()
         {
             return View();
         }
+        [Route("Home/About")]
+        // [Route("Action")]
         public IActionResult About()
         {
             return View();
         }
-
-        public int Details(int id)
+        [Route("Home/Details/{id?}")]  // to make id optional use ?
+        public int Details(int? id)
         {
-            return id;
+            return id ?? 1;
         }
     }
 }
--- ConventionBaseRoutingEmpty/Program.cs
+++ ConventionBaseRoutingEmpty/Program.cs
@@ -1,13 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();       
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 // app.MapDefaultControllerRoute();    this will call home controller 
 //      and its index  action method
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=about}/{id?}");
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=about}/{id?}");
 
+
+app.MapControllers();  // this middleware is used to perform
+                       // attribute base routing
 app.Run();
